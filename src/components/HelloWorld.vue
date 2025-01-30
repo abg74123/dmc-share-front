4e126781c5f6294b8ad03d766d4cddcdaf5387c6
abg74123 <bisza_i.d@hotmail.com>
2025-01-30 15:25:15 +0700

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ import {liff} from "@line/liff";
  console.log("mount")
     try {
       // * Config Line Liff
-      await liff.init({ liffId: "2006835240-4exWlE8E", withLoginOnExternalBrowser: true})
+      await liff.init({ liffId: "2006835556-noawOQEW", withLoginOnExternalBrowser: true})
       // ^ Check Line Liff Login ?
       if (liff.isLoggedIn()) {
         const profile = await liff.getProfile()
