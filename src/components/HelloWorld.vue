8130c664914923a1ca78e80cd9b33b887a9f64a8
abg74123 <bisza_i.d@hotmail.com>
2025-01-30 11:40:48 +0700

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <button>แชร์เลย!!!!!!!!</button>
 </template>
 
-<script>
+<script setup>
 
 import {onMounted} from "vue";
 import {liff} from "@line/liff";
@@ -11,21 +11,23 @@ import {liff} from "@line/liff";
  console.log("mount")
     try {
       // * Config Line Liff
-      await liff.init({ liffId: "2004029731-Akgo4Bm9", withLoginOnExternalBrowser: true })
+      await liff.init({ liffId: "2006835240-LbMy0qgq", withLoginOnExternalBrowser: true })
       // ^ Check Line Liff Login ?
       if (liff.isLoggedIn()) {
         const profile = await liff.getProfile()
         console.log("profile => ", profile)
+           liff.sendMessages({
+    "type": "text",
+    "text": "Hello, world"
+})
         this.loading = false
       } else {
         liff.login()
       }
     } catch (e) {
       console.log("error login => ", e)
+
     }
 })
-export default {
-  name: 'HelloWorld',
-  props: {}
-}
+
 </script>
